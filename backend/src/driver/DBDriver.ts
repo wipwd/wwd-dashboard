6767530ddecd048ac385353d9547b6007f9bb998
Joao Eduardo Luis <joao@wipwd.dev>
2020-10-23 10:31:51 +0000

    backend/db: replace leveldb with mongodb
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- backend/src/driver/DBDriver.ts
+++ backend/src/driver/DBDriver.ts
@@ -6,33 +6,104 @@
  * under the terms of the EUROPEAN UNION PUBLIC LICENSE v1.2, as published by
  * the European Comission.
  */
-import levelup, { LevelUp } from "levelup";
-import leveldown from "leveldown";
 import { assert } from 'console';
-import { Logger } from 'tslog';
+import { Collection, Db, MongoClient } from 'mongodb';
+import { Driver } from './Driver';
 
 
-const logger: Logger = new Logger({name: 'db-driver'});
+export interface DBDriverConfig {
+    user: string;
+    password: string;
+    url: string;
+}
 
+export class DBDriver extends Driver<DBDriverConfig> {
 
-export class DBDriver {
+    private _client?: MongoClient;
+    private _db?: Db;
+    private _is_connected: boolean = false;
 
-    private static instance: DBDriver;
-    private _db: LevelUp;
-
-    private constructor(private _db_path: string) {
-        this._db = levelup(leveldown(_db_path));
+    public constructor() {
+        super("mongodb", true);
     }
 
-    public static open(_db_path: string): void {
-        if (!DBDriver.instance) {
-            logger.info(`opening database at ${_db_path}`);
-            DBDriver.instance = new DBDriver(_db_path);
+    private async _connect(): Promise<void> {
+        if (!this._client) {
+            this.logger.error("can't connect to db; no client.");
+            return;
+        }
+        try {
+            await this._client?.connect();
+            this._db = this._client?.db("wwd-dashboard");
+            this._is_connected = true;
+        } catch (e) {
+            this.logger.info("connected to database");
         }
     }
 
-    public static getDB(): LevelUp {
-        assert(!!DBDriver.instance, "database not open");
-        return DBDriver.instance._db;
+    protected _startup(): boolean {
+        assert(this.hasConfig());
+        assert(!!this._config.user && this._config.user !== "");
+        assert(!!this._config.password && this._config.password !== "");
+        assert(!!this._config.url && this._config.url !== "");
+
+        if (this.hasOpenDBConnection()) {
+            this.logger.warn("won't startup; existing open db");
+            return false;
+        }
+
+        this.logger.debug("config: ", this._config);
+
+        const user: string = this._config.user;
+        const pass: string = this._config.password;
+        const url: string = this._config.url;
+        const options: string = "retryWrites=true&w=majority";
+        const uri: string = `mongodb+srv://${user}:${pass}@${url}/?${options}`;
+        this._client = new MongoClient(uri, {
+            useNewUrlParser: true, useUnifiedTopology: true
+        });
+        this._connect();
+        return true;
+    }
+
+    protected _shutdown(): boolean {
+        this._client?.close();
+        this._is_connected = false;
+        this._db = undefined;
+        return true;
+    }
+
+    protected _shouldUpdateConfig(config: DBDriverConfig): boolean {
+        return (
+            Object.keys(config).length > 0 &&
+            !!config.user && config.user !== "" &&
+            !!config.password && config.password !== "" &&
+            !!config.url && config.url !== "" &&
+            (
+                config.user !== this._config.user ||
+                config.password !== this._config.password ||
+                config.url !== this._config.url
+            )
+        );
+    }
+
+    protected _updatedConfig(): void {
+        this.logger.info("updated config; restart.");
+        if (!this.restart()) {
+            this.logger.error("error restarting mongodb driver");
+        }
+    }
+
+    public hasOpenDBConnection(): boolean {
+        return (!!this._client);
+    }
+
+    public isConnected(): boolean {
+        return (this.hasOpenDBConnection() && this._is_connected);
+    }
+
+    public getCollection<T>(_name: string): Collection<T>|undefined {
+        assert(this.isConnected());
+        return this._db?.collection(_name);
     }
 }
--- backend/src/wwd-dashboard.ts
+++ backend/src/wwd-dashboard.ts
@@ -31,7 +31,13 @@ async function sleep(ms: number): Promise<void> {
 
 async function main(): Promise<void> {
     try {
-        DBDriver.open("./dashboard.db");
+        const _db: DBDriver = new DBDriver();
+        _db.startup();
+        while (!_db.isConnected()) {
+            logger.info("waiting for database to be ready...");
+            await sleep(1000);
+        }
+
         const _http: HTTPDriver = HTTPDriver.getInstance();
         logger.info("starting up...");
         _http.startup();
@@ -42,7 +48,11 @@ async function main(): Promise<void> {
 
         logger.info("shutting down...");
         _http.shutdown();
-
+        _db.shutdown();
+        while (_db.isConnected()) {
+            logger.info("waiting for database shutdown...");
+            await sleep(1000);
+        }
     } catch (e) {
         logger.error("error: ", e);
     }
